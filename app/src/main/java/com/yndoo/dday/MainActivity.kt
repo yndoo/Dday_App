9407ee6c5648927310435451ab0c1766950324cf
yndoo <yndoo2k@gmail.com>
2023-09-20 21:26:18 +0900

    Initial commit

---
+++ app/src/main/java/com/yndoo/dday/MainActivity.kt
@@ -0,0 +1,84 @@
+package com.yndoo.dday
+
+import android.app.DatePickerDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.TextView
+import org.w3c.dom.Text
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.GregorianCalendar
+import java.util.concurrent.TimeUnit
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val startButton = findViewById<Button>(R.id.startBtn)
+        val endButton = findViewById<Button>(R.id.endBtn)
+
+        var startDate = ""
+        var endDate = ""
+        var calendar_start = Calendar.getInstance()
+        var calendar_end = Calendar.getInstance()
+
+        startButton.setOnClickListener {
+
+            val today = GregorianCalendar()
+            val year = today.get(Calendar.YEAR)
+            val month = today.get(Calendar.MONTH)
+            val day = today.get(Calendar.DATE)
+
+            val dlg = DatePickerDialog(this, object: DatePickerDialog.OnDateSetListener{
+                override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+                    startDate = "${year}${month+1}${dayOfMonth}"
+                    Log.d("날짜확인1",startDate)
+                    calendar_start.set(year, month+1, dayOfMonth)
+                    calendar_start.apply {
+                        set(Calendar.HOUR_OF_DAY, 0)
+                        set(Calendar.MINUTE, 0)
+                        set(Calendar.SECOND, 0)
+                        set(Calendar.MILLISECOND, 0)
+                    }
+                }
+
+            }, year, month, day)
+            dlg.show()
+        }
+        endButton.setOnClickListener {
+
+            val today = GregorianCalendar()
+            val year = today.get(Calendar.YEAR)
+            val month = today.get(Calendar.MONTH)
+            val day = today.get(Calendar.DATE)
+
+            val dlg = DatePickerDialog(this, object: DatePickerDialog.OnDateSetListener{
+                override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+                    endDate = "${year}${month+1}${dayOfMonth}"
+                    Log.d("날짜확인2",endDate)
+                    calendar_end.set(year, month+1, dayOfMonth)
+                    calendar_end.apply {
+                        set(Calendar.HOUR_OF_DAY, 0)
+                        set(Calendar.MINUTE, 0)
+                        set(Calendar.SECOND, 0)
+                        set(Calendar.MILLISECOND, 0)
+                    }
+                    val finalDate = TimeUnit.MILLISECONDS.toDays(calendar_end.timeInMillis - calendar_start.timeInMillis) + 1
+                    //val finalDate = (calendar_end.timeInMillis - calendar_start.timeInMillis)/(24*60*60*1000) + 1
+                    Log.d("@@@",TimeUnit.MILLISECONDS.toDays(calendar_start.timeInMillis).toString())
+                    Log.d("@@@",TimeUnit.MILLISECONDS.toDays(calendar_end.timeInMillis).toString())
+
+                    findViewById<TextView>(R.id.ddayText).setText("D+" + finalDate.toString())
+                }
+
+            }, year, month, day)
+            dlg.show()
+
+        }
+
+    }
+}
